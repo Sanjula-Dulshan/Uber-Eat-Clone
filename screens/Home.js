46d4035f1ecc4512527455bb502fcc49d7c760e4
Sanjula-Dulshan <it20253530@my.sliit.lk>
2021-10-14 01:25:31 +0530

    searchBar initial

--- screens/Home.js
+++ screens/Home.js
@@ -2,11 +2,17 @@ import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
 import SafeViewAndroid from "../components/SafeViewAndroid";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-      <HeaderTabs />
+      <View style={{ backgroundColor: "#eee", flex: 1 }}>
+        <View style={{ backgroundColor: "white", padding: 10 }}>
+          <HeaderTabs />
+          <SearchBar />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
