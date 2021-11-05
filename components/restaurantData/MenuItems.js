5adbaf337a414412a43a13a07b8200d61a724683
Sanjula-Dulshan <it20253530@my.sliit.lk>
2021-11-05 16:32:30 +0530

    MenuItems initial

---
+++ components/restaurantData/MenuItems.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { Divider } from "react-native-elements";
+
+const foods = [
+  {
+    title: "Beachside Bar",
+    description: "dsdjfjdv dfnerrfjre dfdgrf ",
+    price: "$3",
+    image_url:
+      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+  },
+  {
+    title: "Benihana",
+    description: "dsdjfjdv dfnerrfjre dfdgrf ",
+    price: "$3",
+    image_url:
+      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  },
+  {
+    title: "India's Grill",
+    description: "dsdjfjdv dfnerrfjre dfdgrf ",
+    price: "$3",
+    image_url:
+      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  },
+  {
+    title: "India's Grill",
+    description: "dsdjfjdv dfnerrfjre dfdgrf ",
+    price: "$3",
+    image_url:
+      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  },
+  {
+    title: "India's Grill",
+    description: "dsdjfjdv dfnerrfjre dfdgrf ",
+    price: "$3",
+    image_url:
+      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+  },
+];
+const styles = StyleSheet.create({
+  menuItemStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20,
+  },
+
+  titleStyle: {
+    fontSize: 19,
+    fontWeight: "bold",
+  },
+});
+export default function MenuItems() {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      {foods.map((food, index) => (
+        <View key={index}>
+          <View style={styles.menuItemStyle}>
+            {/* FoodInfo */}
+            <FoodInfo food={food} />
+
+            {/* FoodImage */}
+            <FoodImage food={food} />
+          </View>
+          <Divider width={0.5} orientation="vertical" />
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const FoodInfo = (props) => (
+  <View style={{ width: 240, justifyContent: "space-evenly" }}>
+    <Text style={styles.titleStyle}>{props.food.title}</Text>
+    <Text>{props.food.description}</Text>
+    <Text>{props.food.price}</Text>
+  </View>
+);
+
+const FoodImage = (props) => (
+  <View>
+    <Image
+      source={{ uri: props.food.image }}
+      style={{ width: 100, height: 100, borderRadius: 8 }}
+    />
+  </View>
+);
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Divider } from "react-native-elements/dist/divider/Divider";
 import About from "../components/restaurantData/About";
+import MenuItem from "../components/restaurantData/MenuItems";
 
 export default function RestaurantDetail() {
   return (
     <View>
       <About />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
+      <MenuItem />
     </View>
   );
 }
