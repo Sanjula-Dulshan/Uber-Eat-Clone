5283786cd736f921cd2c07e5925d10fb74964152
Sanjula-Dulshan <it20253530@my.sliit.lk>
2021-11-05 11:44:50 +0530

    Delivery & pickup tabs completed

--- screens/Home.js
+++ screens/Home.js
@@ -14,6 +14,7 @@ const YELP_API_KEY =
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
   const [city, setCity] = useState("SanDiego");
+  const [activeTab, setActiveTab] = useState("Delivery");
 
   const getRestaurantsFromYelp = () => {
     const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
@@ -25,18 +26,24 @@ export default function Home() {
     };
     return fetch(yelpUrl, apiOptions)
       .then((res) => res.json())
-      .then((json) => setRestaurantData(json.businesses));
+      .then((json) =>
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
   };
 
   useEffect(() => {
     getRestaurantsFromYelp();
-  }, [city]);
+  }, [city, activeTab]);
 
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
       <View style={{ backgroundColor: "#eee", flex: 1 }}>
         <View style={{ backgroundColor: "white", padding: 10 }}>
-          <HeaderTabs />
+          <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
           <SearchBar cityHandler={setCity} />
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
